a94457bb9508cc373cf993fe09f07a46d17911ac
student <Student@B420-01.fesb.hr>
2021-03-22 18:15:37 +0100

    Add project files.

---
+++ Projekt PIS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projekt_PIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
